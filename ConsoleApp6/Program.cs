de360a48de9b379c9aa149a8f8ca5785ad0f4f71
Ahmed <Ahmed@DESKTOP-9N0OBRE>
2022-02-09 17:43:20 +0400

    Add project files.

---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace ConsoleApp6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //int m;
+            //string n = "56";
+            //Console.WriteLine(n);
+            //m = Convert.ToInt32(n);
+            //long l = Convert.ToInt16(n);
+            //int t;
+            //t= int.Parse(n);
+            //Console.WriteLine(m*5);
+            //string num = Console.ReadLine();
+            //double m=Convert.ToDouble(num);
+            //double c = double.Parse(num);
+            //Console.WriteLine("Number: "+c+" Hello");
+            //Console.WriteLine("Number: " + (c+m));
+           // Console.WriteLine("Number {0} Sum: {1}", c, c + m);
+           // Console.WriteLine($"Number {c} Total {c + m}");// string interpolation
+            //int number = int.Parse(Console.ReadLine());
+            //Console.Write("Hello ");
+            //Console.Write("Madina");
+            //double m =Math.Pow(21,2);
+            //Console.WriteLine(Math.Sqrt(441));
+            //Console.WriteLine(Math.Max(2, 5));
+            //Console.WriteLine(Math.Min(2, 5));
+            //Console.WriteLine(Math.Abs(-20));
+            //Console.WriteLine(Math.Floor(5.6));
+            //Console.WriteLine(Math.Ceiling(2.3));
+            //Console.WriteLine(Math.Round(5.4));
+            //Console.WriteLine(Math.Round(5.6));
+            //Console.WriteLine(m);
+
+            //int n = int.Parse(Console.ReadLine());
+            //Console.WriteLine(n / 10 + " " + n % 10);
+            //string text = Console.ReadLine();
+            //long[] nums = Array.ConvertAll(Console.ReadLine().Split(), long.Parse); 
+            //long a = nums[0];
+            //long b = nums[1];
+            //Console.WriteLine(a*a + b*b);
+            int n = int.Parse(Console.ReadLine());
+            n = Math.Abs(n);
+
+            Console.WriteLine(Math.Pow(((n / 10) +(n%10)),2));
+            
+        }
+    }
+}
---
+++ IfElse/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace IfElse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums=Array.ConvertAll(Console.ReadLine().Split(),int.Parse);
+            int x=nums[0];
+            int a=nums[1];
+            int b=nums[2];
+            if (x >= a && x <= b)
+                Console.WriteLine("IN");
+            else
+                Console.WriteLine("OUT");
+                
+        }
+    }
+}

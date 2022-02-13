86efe94bf2a2c9c47e91e9b36e28357966d4b6b7
Ahmed <Ahmed@DESKTOP-9N0OBRE>
2022-02-13 15:39:50 +0400

    String methods

---
+++ ArrayExample/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Linq;
+
+namespace ArrayExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //904
+            /*int n = int.Parse(Console.ReadLine());
+            int[] nums = Array.ConvertAll(Console.ReadLine().Split(),int.Parse);
+            for (int i = 0; i < n-1; i++)
+            {
+                if (nums[i] >= 0) Console.Write((nums[i] + 2) + " ");
+                else Console.Write(nums[i]+ " ");
+            }
+            if (nums[n - 1] >= 0) Console.Write((nums[n - 1] + 2));
+            else Console.Write(nums[n - 1]);*/
+
+            //907
+            /*int n = int.Parse(Console.ReadLine());
+            string[] arr = Console.ReadLine().Split();
+            bool m = true;
+            for(int i = 0; i < n; i++)
+            {
+                if (double.Parse(arr[i]) <= 2.5)
+                {
+                    m = false;
+                    Console.WriteLine("{0} {1:F2)", i + 1, double.Parse(arr[i]));
+                    break;
+                }
+            }
+            if(m) Console.WriteLine("Not Found");*/
+
+            //914
+            //int n = int.Parse(Console.ReadLine());
+            //double[] nums = Array.ConvertAll(Console.ReadLine().Split(), double.Parse);
+            //Console.WriteLine("{0:F2}",Math.Max(Math.Abs(nums.Max()),Math.Abs(nums.Min())));
+
+
+            //917
+            int n = int.Parse(Console.ReadLine());
+            double[] nums = Array.ConvertAll(Console.ReadLine().Split(), double.Parse);
+            Console.WriteLine("{0:F2}", nums.Min() * 2);
+            Array.Sort(nums);
+        }
+    }
+}
---
+++ StringMethods/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace StringMethods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = "Madina Alizada";
+            string subText = text.Substring(3, 3);
+            char[] chars = text.ToCharArray();
+            bool contains = text.Contains("Madina");
+            bool startsWith = text.StartsWith("Al");
+            bool endsWith = text.EndsWith("ada");
+            string paddLeft = text.PadLeft(20);
+            string paddRight = text.PadRight(20);
+            string trim = text.Trim();
+            string trimStart = text.TrimStart();
+            string trimEnd = text.TrimEnd();
+            string toLower = text.ToLower();
+            string toUpper = text.ToUpper();
+            int indexOf = text.IndexOf('a');
+            int lastIndexOf = text.LastIndexOf('M');
+            string remove = text.Remove(3,5);
+            string insert = text.Insert(6, "Hello ");
+            string[] split = text.Split('a','d');
+            string replace = text.Replace('a', 'e').Replace('i','B');
+            // empty=>"" null  whitespace=> "    "
+            text = "    a      ";
+            bool isNullOrEmpty = String.IsNullOrEmpty(text);
+            bool isNullOrWhileSpace = String.IsNullOrWhiteSpace(text);
+            int m = 123465;
+            text = m.ToString();
+            Console.WriteLine($"Result: {text.GetType()}");
+        }
+    }
+}
